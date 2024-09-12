76ad39656cd91d63edd9347560777d55edd2b775
Alaa Tourkey <alaatourkey@gmail.com>
2024-09-12 03:35:29 +0300

    edit in new books

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -598,60 +598,6 @@ function Home() {
       </section>
 
 
-      {/* Choose Demo */}
-
-      <section>
-        <div className="demo py-7">
-          <div className="container w-3/4 m-auto  text-white">
-            <div className="text-center  m-auto mb-16 ">
-              <h2 className='text-5xl my-2 '>Meet Our  <span className='font-bold'>Staff</span></h2>
-              {/* line after header */}
-              <div className="bg-white h-px  min-w-12 w-80 m-auto relative my-6 ">
-                <ul className='absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex'>
-                  <li><i className="fa-solid fa-square  rotate-45 text-xs mx-1 text-white"></i></li>
-                  <li><i className="fa-solid fa-square  rotate-45 text-xs mx-1 text-white"></i></li>
-                </ul>
-              </div>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-10 ">
-              <div className="col-span-1  ">
-                <div className="image-container ">
-                  <img src={demo1} alt="Demo Image" />
-                  <div className="layout bg-blue-700 ">
-                    <h4 className='uppercase'>author</h4>
-                  </div>
-                </div>
-              </div>
-              <div className="col-span-1  ">
-                <div className="image-container ">
-                  <img src={demo2} alt="Demo Image" />
-                  <div className="layout bg-blue-700 ">
-                    <h4 className='uppercase'>author</h4>
-                  </div>
-                </div>
-              </div>
-              <div className="col-span-1  ">
-                <div className="image-container ">
-                  <img src={demo3} alt="Demo Image" />
-                  <div className="layout bg-blue-700 ">
-                    <h4 className='uppercase'>author</h4>
-                  </div>
-                </div>
-              </div>
-              <div className="col-span-1  ">
-                <div className="image-container ">
-                  <img src={demo4} alt="Demo Image" />
-                  <div className="layout bg-blue-700 ">
-                    <h4 className='uppercase'>author</h4>
-                  </div>
-                </div>
-              </div>
-
-            </div>
-          </div>
-        </div>
-      </section>
-
     </>
   )
 }
