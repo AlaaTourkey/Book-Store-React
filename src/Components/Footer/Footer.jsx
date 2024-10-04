0f66d4565c4947e8f4fdaf829091bc4108601a83
Alaa Tourkey <alaatourkey@gmail.com>
2024-10-04 23:08:15 +0300

    edit footer style

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,23 +4,59 @@ import Style from './Footer.module.css'
 function Footer() {
   return (
     <>
-      <footer className="bg-white shadow mt-4 dark:bg-gray-800 fixed bottom-0 w-full">
-        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <div className="text-center w-3/4 md:w-1/2 m-auto my-7">
-            <h2 className='text-5xl my-2'>about <span className='font-bold'>us</span></h2>
-            {/* line after header */}
-            <div className="bg-blue-700 h-px w-auto m-auto relative my-6">
-              <ul className='absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex'>
-                <li><i className="fa-solid fa-square text-blue-700 rotate-45 text-xs mx-1"></i></li>
-                <li><i className="fa-solid fa-square text-blue-700 rotate-45 text-xs mx-1"></i></li>
-              </ul>
+      <div className="  mt-4 bg-zinc-800 w-full py-8">
+        <div className="grid grid-cols-3 gap-10 w-3/4 mx-auto ">
+          <div className="col-span-1">
+            <div className="">
+              <div className="">
+                <p className='min-h-5 max-w-80'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut gravida, quam vitae est Sed non eros elementum nulla sodales ullamcorper.</p>
+              </div>
+              <div className="icons my-3">
+                <i className="fa-brands fa-twitter text-gray-300 me-3 hover:text-blue-700 duration-300"></i>
+                <i className="fa-brands fa-linkedin text-gray-300 mx-3 hover:text-blue-700 duration-300"></i>
+                <i className="fa-brands fa-facebook text-gray-300 mx-3 hover:text-blue-700 duration-300"></i>
+                <i className="fa-brands fa-instagram text-gray-300 mx-3 hover:text-blue-700 duration-300"></i>
+              </div>
+              <div className="">
+                <ul className=' flex'>
+                  <li><i className="fa-solid fa-square rotate-45  text-xs mx-1 text-gray-300"></i></li>
+                  <li><i className="fa-solid fa-square rotate-45  text-xs mx-1 text-gray-300"></i></li>
+                  <li><i className="fa-solid fa-square rotate-45  text-xs mx-1 text-gray-300"></i></li>
+                </ul>
+              </div>
             </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut gravida, quam vitae est Sed non eros elementum nulla sodales ullamcorper.</p>
           </div>
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
-          </span>
+          <div className="col-span-1">
+            <div className="">
+              <h1>Contact Us</h1>
+              <table className="w-full  ">
+                <tbody>
+                  <tr className='align-top'>
+                    <td className='align-top'><i className="fa-solid fa-location-dot text-gray-300 mx-3 hover:text-blue-700 duration-300"></i></td>
+                    <td className='align-top'>3050 Universal Blvd #190 Fort Lauderdale, FL, 33331, Sydney New York City</td>
+                  </tr>
+                  <tr>
+                    <td><i className="fa-solid fa-location-dot text-gray-300 mx-3 hover:text-blue-700 duration-300"></i></td>
+                    <td>susislibrary@domain.com</td>
+                  </tr>
+                  <tr>
+                    <td><i className="fa-solid fa-location-dot text-gray-300 mx-3 hover:text-blue-700 duration-300"></i></td>
+                    <td>+62 582 528 527 21</td>
+                  </tr>
+                  <tr>
+                    <td><i className="fa-solid fa-location-dot text-gray-300 mx-3 hover:text-blue-700 duration-300"></i></td>
+                    <td>www.susislibrary.com</td>
+                  </tr>
+                </tbody>
+              </table>
+
+            </div>
+          </div>
+          <div className="col-span-1">
+
+          </div>
         </div>
-      </footer>
+      </div>
 
     </>
   )
